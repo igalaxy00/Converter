b7d4aa4a43738fe648d113485980a741f40912fe
Igor Galaktionov <i.galaxy00@gmail.com>
2022-11-27 13:17:51 +0300

    #2 c3 add minutes to seconds impl

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -6,14 +6,27 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 val DEGREES = "degrees"
+val MINUTES = "minutes"
 fun Application.configureRouting() {
 
     routing {
-        get("/convert") {
+        get("/convert/degree") {
             val degree = call.parameters[DEGREES]
             call.respondText("Hello World!")
         }
+
+        get("/convert/minute") {
+            val minute = call.parameters[MINUTES]
+            if (minute == null) {
+                call.respondText("ERROR EMPTY PARAMETER")
+            } else {
+                val out = "Seconds are " + (minute.toInt() * 60).toString()
+                call.respondText(out)
+            }
+        }
+
     }
+
 
 
 
